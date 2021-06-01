fbafa26172091dcafa0d6533e81b9166dc01fe02
jakelw96 <jake.lw96@gmail.com>
2021-05-31 23:45:22 -0600

    Working on connection to db. Started prompt questions

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-DROP DATABASE IF EXISTS employe
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+// Connect to database
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to the database.');
+});
+
+const sql = `SELECT * FROM employee;`
+
+const data = db.query(sql, (err, result) => {
+    if (err) {
+        throw err;
+    }
+    return result
+});
+console.log(data)
+
+// const cTable = require('console.table');
+// console.table([
+//     {
+//         name: 'foo',
+//         age: 10
+//     },
+//     {
+//         name: 'bar',
+//         age: 20
+//     }
+// ]);
+
+// Initial options at start of application
+const initialPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: '1stPrompt',
+            message: "What would you like to do?",
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
+        }
+    ]);
+};
+
+
+
+//initialPrompt();
