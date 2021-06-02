b2ff665866cf52dfdd00a86f1744243ca12241de
jakelw96 <jake.lw96@gmail.com>
2021-06-01 23:11:57 -0600

    Completed functions to get data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
-const viewAllEmployees = require('./lib/getData/index');
+const cTable = require('console.table');
+
 
 
 // Connect to database
@@ -9,7 +10,7 @@ db.connect(err => {
     console.log('Connected to the database.');
 });
 
-//Initial options at start of application
+// Initial options at start of application
 const initialPrompt = () => {
     return inquirer.prompt([
         {
@@ -30,15 +31,81 @@ const initialPrompt = () => {
     ])
     .then(response => {
         const answer = (response.prompt).toString();
+
         if (answer === 'View All Employees') {
             viewAllEmployees();
-            initialPrompt();
+        } else if (answer === 'View All Departments') {
+             viewAllDepartments();
+        } else if (answer === 'View All Roles') {
+             viewAllRoles();
+        } else if (answer === 'Add A Department') {
+            // addDepartment()
+        } else if (answer === 'Add A Role') {
+            // addRole()
+        } else if (answer === 'Add An Employee') {
+            // addEmployee()
+        } else if (answer === 'Update An Employee Role') {
+            // updateRole()
         } else if (answer === 'Exit') {
             db.end();
         }
     });
 };
 
+// Gets all employee data from database
+const viewAllEmployees = () => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name,
+                role.title AS title,
+                department.name AS department, role.salary,
+                CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN (employee manager) ON manager.id = employee.manager_id
+                LEFT JOIN role on employee.role_id = role.id
+                LEFT JOIN department ON department.id = role.department_id;`
 
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(``);
+        console.log(``);
+        console.log(`                                   ` + 'All Employees');
+        console.log(`=============================================================================================`)
+        console.table(result);
+        console.log(`=============================================================================================`)
+        initialPrompt();
+    });
+};
+
+// Gets all department data from database
+const viewAllDepartments = () => {
+    const sql = `SELECT * FROM department;`
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(``);
+        console.log(``);
+        console.log(`                   ` + 'All Departments');
+        console.log(`========================================================`)
+        console.table(result);
+        console.log(`========================================================`)
+        initialPrompt();
+    });
+};
+
+// Gets all role data from database
+const viewAllRoles = () => {
+    const sql = `SELECT role.id, role.title, role.salary, department.name AS department
+                FROM role
+                LEFT JOIN department ON department.id = role.department_id;`
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(``);
+        console.log(`                     ` + 'All Roles');
+        console.log(`========================================================`)
+        console.table(result);
+        console.log(`========================================================`);
+        initialPrompt();
+    });
+};
 
 initialPrompt();
